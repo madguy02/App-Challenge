ef857097a2a4daa4069412c0d28e1c7697d84fab
madguy02 <manishkakoti688@gmail.com>
2019-03-31 11:27:33 +0530

    added models

---
+++ models/app.models.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const AppSchema = mongoose.Schema({
+    title: String,
+    story: String,
+},
+{
+    timestamps: true  
+});
+
+module.exports = mongoose.model('Inkredo', AppSchema);

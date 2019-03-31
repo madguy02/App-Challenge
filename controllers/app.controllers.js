9c9737eae2dbb6e689f1bf0cad54ef14b80a744b
madguy02 <manishkakoti688@gmail.com>
2019-03-31 12:32:47 +0530

    updated code on update and delete

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,6 +1,8 @@
 const inkredo = require('../models/app.models.js');
 
+// create API 
 exports.create = (req, res) => {
+
 const content = new inkredo({
     title: req.body.title,
     story: req.body.story
@@ -16,4 +18,69 @@ content.save()
 });
 
 }
+// find all API for stories
+
+exports.findAll = (req, res) => {
+    inkredo.find()
+    .then(story => {
+        res.send(story);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || 'Some error occured while retrieving files'
+        });
+    });
+}
+
+// update API
+
+exports.update = (req, res) => {
+    
+    inkredo.findByIdAndUpdate(req.params.storyId, {
+        title: req.body.title,
+        story: req.body.story
+    }, {new: true})
+    .then(stories => {
+        if(!stories) {
+            return res.status(404).send({
+                message: 'Story not found with id ' + req.params.storyId
+            });
+        }
+        res.send(stories);
+
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: 'Story not found with id ' + req.params.storyId
+            });                
+        }
+        return res.status(500).send({
+            message: 'Error updating story with id ' + req.params.storyId
+        });
+    });
+
+}
+
+// delete API
+exports.delete = (req, res) => {
+    inkredo.findByIdAndRemove(req.params.storyId)
+    .then(stories => {
+        if(!stories) {
+            return res.status(404).send({
+                message: 'Story not found with id ' + req.params.storyId
+            });
+        }
+        res.send({message: 'Story deleted successfully!'});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: 'Story not found with id ' + req.params.storyId
+            });                
+        }
+        return res.status(500).send({
+            message: 'Could not delete story with id ' + req.params.storyId
+        });
+    });
+
+}
+
 
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -2,5 +2,8 @@ module.exports = (app) => {
     const story = require('../controllers/app.controllers.js');
    
     app.post('/addstory', story.create);
+    app.get('/story',story.findAll);
+    app.put('/story/:storyId',story.update);
+    app.delete('/story/:storyId', story.delete);
 
 }
